f288a7429d47532d12804bcb826b54a4053e9e76
ConnorTron110 <connorcrook1912@gmail.com>
2022-01-31 00:39:30 +0000

    SCP-002 Bed Death Block Event

--- src/main/java/com/drevery/scpdisturbance/ModDamageSources.java
+++ src/main/java/com/drevery/scpdisturbance/ModDamageSources.java
@@ -3,5 +3,5 @@ package com.drevery.scpdisturbance;
 import net.minecraft.util.DamageSource;
 
 public class ModDamageSources {
-    public static final DamageSource SCP_002 = new DamageSource("002_damage");
+    public static final DamageSource SCP_002_BED = new DamageSource("002_bed").setDamageIsAbsolute().setDamageBypassesArmor();
 }
--- src/main/java/com/drevery/scpdisturbance/events/CommonEvents.java
+++ src/main/java/com/drevery/scpdisturbance/events/CommonEvents.java
@@ -1,16 +1,42 @@
 package com.drevery.scpdisturbance.events;
 
+import com.drevery.scpdisturbance.ModDamageSources;
 import com.drevery.scpdisturbance.SCPDisturbance;
+import com.drevery.scpdisturbance.block.ModBlocks;
 import com.drevery.scpdisturbance.commands.ReturnHomeCommand;
 import com.drevery.scpdisturbance.commands.SetHomeCommand;
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.server.command.ConfigCommand;
 
+import java.util.ArrayList;
+
 @Mod.EventBusSubscriber(modid = SCPDisturbance.MOD_ID)
 public class CommonEvents { //Forge Events used on normal events IE. LivingDeathEvent
+
+    static final ArrayList<Block> SCP002_BED_REPLACEABLE = new ArrayList<>();
+    static {
+        SCP002_BED_REPLACEABLE.add(ModBlocks.FLOOR_TRAP.get());
+        SCP002_BED_REPLACEABLE.add(ModBlocks.SKIN_LAMP.get());
+        SCP002_BED_REPLACEABLE.add(ModBlocks.SKIN_STAND.get());
+        SCP002_BED_REPLACEABLE.add(ModBlocks.BEAR.get());
+    }
+
+    @SubscribeEvent
+    public static void onLivingDeathEvent(LivingDeathEvent event) {
+        if (event.getSource() == ModDamageSources.SCP_002_BED) {
+            Entity entity = event.getEntity();
+            if (entity.getEntityWorld().isAirBlock(entity.getPosition())) {
+                entity.getEntityWorld().setBlockState(entity.getPosition(),
+                        SCP002_BED_REPLACEABLE.get(entity.getEntityWorld().rand.nextInt(SCP002_BED_REPLACEABLE.size())).getDefaultState());
+            }
+        }
+    }
 
     @SubscribeEvent
     public static void onPlayerCloneEvent(PlayerEvent.Clone event) {
