f67f20ac32d510b55c4d97abebbf9d3ceb4218a9
ConnorTron110 <connorcrook1912@gmail.com>
2022-02-02 00:50:50 +0000

    Added SCP-002 sinking into skin related events One model change and new damage source

--- src/main/java/com/drevery/scpdisturbance/block/custom/FaceSkinFloor.java
+++ src/main/java/com/drevery/scpdisturbance/block/custom/FaceSkinFloor.java
@@ -1,10 +1,10 @@
 package com.drevery.scpdisturbance.block.custom;
 
+import com.drevery.scpdisturbance.ModDamageSources;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
@@ -14,8 +14,9 @@ public class FaceSkinFloor extends Block {
         super(properties);
     }
 
+    @Override
     public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
         entityIn.setMotionMultiplier(state, new Vector3d(0.25D, 1D, 0.25D));
-        entityIn.attackEntityFrom(DamageSource.GENERIC, 4F);
+        entityIn.attackEntityFrom(ModDamageSources.SCP_002_SINK, 4F);
     }
 }
