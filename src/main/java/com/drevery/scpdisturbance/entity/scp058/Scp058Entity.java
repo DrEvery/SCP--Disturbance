4f693cba3514c128984fbd4b8fc3e46f6d6f3a16
ConnorTron110 <connorcrook1912@gmail.com>
2022-03-08 02:37:01 +0000

    Added Entity related helper code Added Attributions to the entity registration file Entity Eggs are now simple to add

--- src/main/java/com/drevery/scpdisturbance/entity/scp058/Scp058Entity.java
+++ src/main/java/com/drevery/scpdisturbance/entity/scp058/Scp058Entity.java
@@ -2,9 +2,6 @@ package com.drevery.scpdisturbance.entity.scp058;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.MobEntity;
-import net.minecraft.entity.ai.attributes.AttributeModifierMap;
-import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.monster.SpiderEntity;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
@@ -15,14 +12,6 @@ public class Scp058Entity extends SpiderEntity {
 
     public Scp058Entity(EntityType<? extends SpiderEntity> type, World worldIn) {
         super(type, worldIn);
-    }
-
-    public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
-        return MobEntity.createMobAttributes()
-                .add(Attributes.MAX_HEALTH, 150.0D)
-                .add(Attributes.MOVEMENT_SPEED, 2D)
-                .add(Attributes.ATTACK_DAMAGE, 4.5D)
-                .add(Attributes.FOLLOW_RANGE, 50.0D);
     }
 
     @Override //No point keeping this method if we don't intend to change AI
--- src/main/java/com/drevery/scpdisturbance/entity/scp058/Scp058TentacleEntity.java
+++ src/main/java/com/drevery/scpdisturbance/entity/scp058/Scp058TentacleEntity.java
@@ -2,9 +2,6 @@ package com.drevery.scpdisturbance.entity.scp058;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.MobEntity;
-import net.minecraft.entity.ai.attributes.AttributeModifierMap;
-import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
@@ -15,17 +12,6 @@ public class Scp058TentacleEntity extends MonsterEntity {
 
     public Scp058TentacleEntity(EntityType<? extends MonsterEntity> type, World worldIn) {
         super(type, worldIn);
-    }
-
-    public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
-        return MobEntity.createMobAttributes()
-                .add(Attributes.MAX_HEALTH, 20.0D)
-                .add(Attributes.MOVEMENT_SPEED, 0D)
-                .add(Attributes.ATTACK_DAMAGE, 4.5D)
-                .add(Attributes.FOLLOW_RANGE, 1D)
-                .add(Attributes.KNOCKBACK_RESISTANCE, 100D)
-                .add(Attributes.ATTACK_KNOCKBACK, 3D)
-                .add(Attributes.ATTACK_SPEED, 1.5D);
     }
 
     @Override
