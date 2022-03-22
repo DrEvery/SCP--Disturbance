0a1595c746092ae430165d8535ccc4104295bba9
ZeusIGN <ZeusCovert@gmail.com>
2022-03-23 00:32:55 +0200

    Changes.

--- src/main/java/com/drevery/scpdisturbance/entity/scp529/JosieEntity.java
+++ src/main/java/com/drevery/scpdisturbance/entity/scp529/JosieEntity.java
@@ -3,16 +3,33 @@ package com.drevery.scpdisturbance.entity.scp529;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
 public class JosieEntity extends CatEntity {
-    public JosieEntity(EntityType<? extends CatEntity> type, World worldIn){
+    public JosieEntity(EntityType<? extends CatEntity> type, World worldIn) {
         super(type, worldIn);
     }
 
     @Override
     public boolean canMate(AnimalEntity p_70878_1_) {
         return false;
+    }
+
+    @Override
+    public void setTame(boolean pTamed) {
+    }
+
+    @Override
+    public void tame(PlayerEntity pPlayer) {
+    }
+
+    @Override
+    public ActionResultType mobInteract(PlayerEntity pPlayer, Hand pHand) {
+    return ActionResultType.FAIL;
     }
 }
 
