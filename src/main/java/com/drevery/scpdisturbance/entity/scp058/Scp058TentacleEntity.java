84041a417103713e2558956dec0c3ede6b5a3660
ZeusIGN <ZeusCovert@gmail.com>
2022-03-24 11:25:54 +0200

    Changed AI for 058 Tentacle.

--- src/main/java/com/drevery/scpdisturbance/entity/scp058/Scp058TentacleEntity.java
+++ src/main/java/com/drevery/scpdisturbance/entity/scp058/Scp058TentacleEntity.java
@@ -1,26 +1,42 @@
 package com.drevery.scpdisturbance.entity.scp058;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.CreatureEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.ZombieAttackGoal;
 import net.minecraft.entity.monster.CaveSpiderEntity;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.entity.monster.SpiderEntity;
+import net.minecraft.entity.passive.AmbientEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 
-public class Scp058TentacleEntity extends CaveSpiderEntity {
+public class Scp058TentacleEntity extends MonsterEntity {
 
-    public Scp058TentacleEntity(EntityType<? extends CaveSpiderEntity> type, World worldIn) {
+    public Scp058TentacleEntity(EntityType<? extends MonsterEntity> type, World worldIn) {
         super(type, worldIn);
     }
 
     @Override
     protected void registerGoals() {
-        super.registerGoals();
+        this.goalSelector.addGoal(1, new LookAtGoal(this, PlayerEntity.class, 8.0F));
+        this.addBehaviourGoals();
     }
-
+    protected void addBehaviourGoals() {
+        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 5.0D, false));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
+    }
     //TODO Sounds
     @Override
     protected SoundEvent getAmbientSound() {
@@ -39,6 +55,23 @@ public class Scp058TentacleEntity extends CaveSpiderEntity {
 
     @Override
     protected void playStepSound(BlockPos pPos, BlockState pBlock) {
+    }
+    public boolean doHurtTarget(Entity pEntity) {
+        if (super.doHurtTarget(pEntity)) {
+            if (pEntity instanceof LivingEntity) {
+                int i = 1;
+                if (this.level.getDifficulty() == Difficulty.NORMAL) {
+                    i = 7;
+                } else if (this.level.getDifficulty() == Difficulty.HARD) {
+                    i = 15;
+                }
+                    ((LivingEntity)pEntity).addEffect(new EffectInstance(Effects.WITHER, i * 20, 0));
+                }
+
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
