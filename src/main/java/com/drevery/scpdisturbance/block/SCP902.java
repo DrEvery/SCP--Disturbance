98bc044302de98e95a4704e4c069bc108e4a2e40
DrEvery <omeguswatchtower@gmail.com>
2022-04-16 17:00:46 -0600

    Started working on SCP-902's functionalities.

--- src/main/java/com/drevery/scpdisturbance/block/SCP902.java
+++ src/main/java/com/drevery/scpdisturbance/block/SCP902.java
@@ -4,11 +4,16 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalBlock;
+import net.minecraft.entity.monster.piglin.PiglinTasks;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.StateContainer;
+import net.minecraft.stats.Stat;
+import net.minecraft.stats.Stats;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
@@ -26,8 +31,4 @@ public class SCP902 extends HorizontalBlock {
         return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
-    @Override
-    public ActionResultType use(BlockState pState, World pLevel, BlockPos pPos, PlayerEntity pPlayer, Hand pHand, BlockRayTraceResult pHit) {
-        return ActionResultType.SUCCESS;  //NOT FINISHED, DO NOT JUDGE ANYTHING HERE AND FOR THE LOVE OF GOD DON'T TOUCH IT FOR IT MIGHT EXPLODE!!!
-    }
 }
