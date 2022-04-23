55a5b80e094fb90a3a15fe0c167d92efe96ee76a
ZeusIGN <ZeusCovert@gmail.com>
2022-04-23 19:25:42 +0300

    NVG fix

--- src/main/java/com/drevery/scpdisturbance/client/model/NVGModel.java
+++ src/main/java/com/drevery/scpdisturbance/client/model/NVGModel.java
@@ -26,16 +26,16 @@ public class NVGModel<T extends LivingEntity> extends HumanoidModel<T> {
         MeshDefinition meshdefinition = HumanoidModel.createMesh(CubeDeformation.NONE, 0.0F);
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition Root = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(12, 3).addBox(-0.5F, -29.0F, -8.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 9).addBox(-4.0F, -30.0F, -7.0F, 8.0F, 4.0F, 3.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 16).addBox(-5.0F, -29.0F, -4.0F, 1.0F, 2.0F, 8.0F, new CubeDeformation(0.0F))
-                .texOffs(14, 8).addBox(4.0F, -29.0F, -4.0F, 1.0F, 2.0F, 8.0F, new CubeDeformation(0.0F))
-                .texOffs(12, 0).addBox(-4.0F, -29.0F, 4.0F, 8.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 0).addBox(-1.0F, -32.0F, 4.0F, 2.0F, 3.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 4).addBox(-1.0F, -32.0F, -5.0F, 2.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 0).addBox(-1.0F, -33.0F, -4.0F, 2.0F, 1.0F, 8.0F, new CubeDeformation(0.0F))
-                .texOffs(15, 4).addBox(-3.0F, -28.0F, -8.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(12, 5).addBox(2.0F, -28.0F, -8.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+        PartDefinition Root = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(12, 3).addBox(-0.5F, -5.0F, -8.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 9).addBox(-4.0F, -6.0F, -7.0F, 8.0F, 4.0F, 3.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 16).addBox(-5.0F, -5.0F, -4.0F, 1.0F, 2.0F, 8.0F, new CubeDeformation(0.0F))
+                .texOffs(14, 8).addBox(4.0F, -5.0F, -4.0F, 1.0F, 2.0F, 8.0F, new CubeDeformation(0.0F))
+                .texOffs(12, 0).addBox(-4.0F, -5.0F, 4.0F, 8.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-1.0F, -8.0F, 4.0F, 2.0F, 3.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 4).addBox(-1.0F, -8.0F, -5.0F, 2.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-1.0F, -9.0F, -4.0F, 2.0F, 1.0F, 8.0F, new CubeDeformation(0.0F))
+                .texOffs(15, 4).addBox(-3.0F, -4.0F, -8.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(12, 5).addBox(2.0F, -4.0F, -8.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
         return LayerDefinition.create(meshdefinition, 32, 32);
     }
